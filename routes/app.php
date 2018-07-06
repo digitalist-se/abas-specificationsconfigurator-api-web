521f68ef5456797f2555ae39df75f0da7fcb5e30
Dimitri Pfaffenrodt <dimitri.pfaffenrodt@gal-digital.de>
2018-07-06 15:52:41 +0200

    [BUGFIX] wrong route to register account

--- routes/app.php
+++ routes/app.php
@@ -10,5 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/register')->name('register');
+Route::get('/create')->name('register');
 Route::get('/login')->name('login');
