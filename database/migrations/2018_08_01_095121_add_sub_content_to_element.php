2b25ce57cfb14f0a92e55842028f42bed961f6a9
Dimitri Pfaffenrodt <dimitri.pfaffenrodt@gal-digital.de>
2018-08-01 11:53:12 +0200

    [FEATURE] add sub_content field to elements

---
+++ database/migrations/2018_08_01_095121_add_sub_content_to_element.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubContentToElement extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('elements', function (Blueprint $table) {
+            $table->string('sub_content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('elements', function (Blueprint $table) {
+            $table->dropColumn('sub_content');
+        });
+    }
+}
