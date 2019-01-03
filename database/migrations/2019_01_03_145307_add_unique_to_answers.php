7516fd6778b47bbbc12b44a21007963dfe2fc534
Dimitri Pfaffenrodt <dimitri.pfaffenrodt@gal-digital.de>
2019-01-03 16:31:36 +0100

    [BUGFIX] answers are not unique

---
+++ database/migrations/2019_01_03_145307_add_unique_to_answers.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUniqueToAnswers extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        DB::transaction(function () {
+            DB::table('answers')->selectRaw('GROUP_CONCAT(id ORDER BY updated_at DESC SEPARATOR \',\') as ids')
+                ->groupBy('element_id', 'user_id')
+                ->havingRaw('count(1) > ?', [1])->get()->each(function ($result) {
+                    $ids = explode(',', $result->ids);
+                    if (count($ids) > 1) {
+                        unset($ids[0]); // not delete first;
+                        DB::table('answers')->whereIn('id', $ids)->delete();
+                    }
+                });
+            Schema::table('answers', function (Blueprint $table) {
+                $table->unique(['user_id', 'element_id']);
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            $table->dropUnique(['user_id', 'element_id']);
+        });
+    }
+}
