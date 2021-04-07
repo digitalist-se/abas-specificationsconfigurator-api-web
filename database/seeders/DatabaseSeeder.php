38b41d427004b37b0acace39c2cda8924eb3a8f8
Hagen Pommer <hagen.pommer@gal-digital.de>
2021-04-07 15:09:12 +0200

    :mute: remove log of seeding durations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,34 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $seedingTimes = [];
-
-        $start = hrtime(true);
-
         $this->call(DocumentTextsSeeder::class);
-        $end = hrtime(true);
-        $seedingTimes['DocumentTextsSeeder'] = ($end - $start) * 1e-9;
-        $start = hrtime(true);
         $this->call(AdminSeeder::class);
-        $end = hrtime(true);
-        $seedingTimes['AdminSeeder'] = ($end - $start) * 1e-9;
-        $start = hrtime(true);
         $this->call(ChoiceTypeSeeder::class);
-        $end = hrtime(true);
-        $seedingTimes['ChoiceTypeSeeder'] = ($end - $start) * 1e-9;
-        $start = hrtime(true);
         $this->call(ElementSeeder::class);
-        $end = hrtime(true);
-        $seedingTimes['ElementSeeder'] = ($end - $start) * 1e-9;
-        $start = hrtime(true);
         $this->call(DemoUserSeeder::class);
-        $end = hrtime(true);
-        $seedingTimes['DemoUserSeeder'] = ($end - $start) * 1e-9;
-        $start = hrtime(true);
         $this->call(BlacklistedEmailDomainSeeder::class);
-        $end = hrtime(true);
-        $seedingTimes['BlacklistedEmailDomainSeeder'] = ($end - $start) * 1e-9;
-
-        Log::debug('Seeding Times', $seedingTimes);
     }
 }
