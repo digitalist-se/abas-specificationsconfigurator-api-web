5c31fc017b45276e7d3fbfe7bc5138506a3e3e16
Pfaffenrodt <dimitri@pfaffenrodt.de>
2022-03-03 13:46:41 +0100

    :bug: incorrect method for verification.resend route

--- routes/authApi.php
+++ routes/authApi.php
@@ -11,9 +11,10 @@ use App\Http\Controllers\SectionController;
 use App\Http\Controllers\StructureController;
 use App\Http\Controllers\TextController;
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/email/resend', function (Request $request) {
+Route::post('/email/resend', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
 
     return response()->noContent();
