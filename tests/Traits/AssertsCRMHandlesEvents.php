ef12fe5a935880077c3765cff8cc8bfb938b978c
Hagen Pommer <hagen.pommer@gal-digital.de>
2022-09-19 09:45:02 +0200

    :test_tube: expect custom contact properties at specific events
    abas-software/abas-specificationsconfigurator-api-web#43

--- tests/Traits/AssertsCRMHandlesEvents.php
+++ tests/Traits/AssertsCRMHandlesEvents.php
@@ -14,35 +14,31 @@ trait AssertsCRMHandlesEvents
     protected function assertCRMServiceHandlesUserRegistered(MockInterface $crmService, array $userAttributes): void
     {
         $userPhone = Arr::get($userAttributes, 'phone');
-        $expectUserAndContactType = fn (User $user, ContactType $type) => $user->phone === $userPhone && $type === ContactType::User;
         $crmService
             ->shouldReceive('upsertContact')
-            ->withArgs($expectUserAndContactType)
+            ->withArgs(fn (User $u, ContactType $type, array $props) => $u->phone === $userPhone && $type === ContactType::User && $props === ['erp_registration_trigger' => true])
             ->andReturn(true);
 
-        $expectEvent = fn (Registered $event) => ($user = $event->user) instanceof User && $userPhone === $user->phone;
         $crmService->shouldReceive('trackUserRegistered')
-            ->withArgs($expectEvent)
+            ->withArgs(fn (Registered $event) => ($user = $event->user) instanceof User && $userPhone === $user->phone)
             ->andReturn(true);
     }
 
     protected function assertCRMServiceHandlesExportedDocument(MockInterface $crmService, User $user): void
     {
-        $expectUser = fn (User $u) => $u->id === $user->id;
-        $expectUserAndContactType = fn (ContactType $contactType) => fn (User $u, ContactType $type) => $u->id === $user->id && $type === $contactType;
         $crmService
             ->shouldReceive('upsertContact')
-            ->withArgs($expectUserAndContactType(ContactType::User))
+            ->withArgs(fn (User $u, ContactType $type, array $props) => $u->id === $user->id && $type === ContactType::User && $props === ['erp_lastenheft_trigger' => true])
             ->andReturn(true);
 
         $crmService
             ->shouldReceive('updateCompany')
-            ->withArgs($expectUser)
+            ->withArgs(fn (User $u) => $u->id === $user->id)
             ->andReturn(true);
 
         $crmService
             ->shouldReceive('upsertContact')
-            ->withArgs($expectUserAndContactType(ContactType::Company))
+            ->withArgs(fn (User $u, ContactType $type, array $props) => $u->id === $user->id && $type === ContactType::Company && empty($props))
             ->andReturn(true);
 
         $expectEvent = fn (ExportedDocument $event) => $event->user->id === $user->id;
