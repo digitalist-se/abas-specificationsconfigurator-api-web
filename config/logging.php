20892736052028de3b0f4cca171002fd1fb6b332
Hagen Pommer <hagen.pommer@gal-digital.de>
2025-08-21 10:25:12 +0200

    🔧 (logs) switch to daily logs keeping it 4 weeks

--- config/logging.php
+++ config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver'            => 'stack',
-            'channels'          => ['single'],
+            'channels'          => ['daily'],
             'ignore_exceptions' => false,
         ],
 
@@ -65,7 +65,7 @@ return [
             'driver' => 'daily',
             'path'   => storage_path('logs/laravel.log'),
             'level'  => env('LOG_LEVEL', 'debug'),
-            'days'   => 7,
+            'days'   => 28,
         ],
 
         'slack' => [
